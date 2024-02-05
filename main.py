9a6c4b1478a6a61e10445e457640b193dfeb3264
Maazin Kazi <maazinazim@gmail.com>
2024-02-05 22:35:47 +0530

    got bored

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ pygame.init()
 # This bit creates the screen of [height x width] pixels
 screen = pygame.display.set_mode((800,600))
 
+# The title and icon for the game
+pygame.display.set_caption("SMR")
+icon = pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
+
 
 # Event is anything that happens inside the game window
 
@@ -15,4 +20,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            #dsbsuobjdsbjodgs
+    
+    screen.fill((0,255,0)) # These are RGB values. bound - {0,255}. Stored in tuples
+    pygame.display.update()
